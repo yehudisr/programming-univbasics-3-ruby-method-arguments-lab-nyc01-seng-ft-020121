7bc75efd21e1653c41e63dafa4adeed7ce84c837
yehudis raitman <yehudisr@gmail.com>
2021-01-13 17:59:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,9 +7,9 @@ def introduction_with_language (name, language)
     "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
-
-
+def introduction_with_language_optional (name, language = 'Ruby')
+ "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
